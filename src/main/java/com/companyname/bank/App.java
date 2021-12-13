ba16b2066a9ffaf3c367d57c703563f51ff60a7d
JayaOps <94829587+JayaOps@users.noreply.github.com>
2021-12-13 11:08:13 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "updated commit commit");
+        System.out.println( "webhook-git commit");
     }
 }
